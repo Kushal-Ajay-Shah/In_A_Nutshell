69a9cca5d8c9c39a0f3ce8bb9eb861d24b615984
MannDoshi <manndoshi19@gmail.com>
2022-03-24 20:38:43 +0530

    using T5TokenizerFast instead of T5Tokenizer

--- scripts/t5_summary.py
+++ scripts/t5_summary.py
@@ -1,4 +1,5 @@
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import T5TokenizerFast, T5ForConditionalGeneration
+import re
 
 """
 Download following files:
@@ -15,9 +16,20 @@ T5_PATH = './model/t5-base' # T5 model path
 # initialize the model architecture and weights
 t5_model = T5ForConditionalGeneration.from_pretrained(T5_PATH)
 # initialize the model tokenizer
-t5_tokenizer = T5Tokenizer.from_pretrained(T5_PATH)
+t5_tokenizer = T5TokenizerFast.from_pretrained(T5_PATH)
+
+def deEmojify(text):
+    regrex_pattern = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags = re.UNICODE)
+    return regrex_pattern.sub(r'',text)
 
 def getSummary(input):
+    input = deEmojify(input)
+    # print("DE-EMOJIFY: ",input)
     # encode the text into tensor of integers using the tokenizer
     inputs = t5_tokenizer.encode("summarize: " + input, return_tensors="pt", max_length=512, padding="max_length", truncation=True)
     summary_ids = t5_model.generate(inputs, num_beams=int(2), no_repeat_ngram_size=3, length_penalty=2, min_length=120, max_length=150, early_stopping=True)
