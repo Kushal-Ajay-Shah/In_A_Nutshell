e52e7c339f788c4d12a3e5638145704bf7a47b44
MannDoshi <manndoshi19@gmail.com>
2022-08-13 15:54:11 +0530

    modified t5_summary to make  chunks of input text

--- scripts/utils/summary/t5_summary.py
+++ scripts/utils/summary/t5_summary.py
@@ -7,7 +7,6 @@ Download following files:
 2. pytorch_model.bin
 3. spiece.model
 4. tokenizer.json
-
 Make a directory named 't5-base' inside 'model' 
 Place the downloaded files inside 't5-base' directory 
 Link: https://huggingface.co/t5-base/tree/main
@@ -27,6 +26,13 @@ def deEmojify(text):
                            "]+", flags = re.UNICODE)
     return regrex_pattern.sub(r'',text)
 
+def pre_process(input):
+    input = deEmojify(input)
+    input = input.replace('.', '.<eos>')
+    input = input.replace('?', '?<eos>')
+    input = input.replace('!', '!<eos>')
+    return input
+
 def post_process(output):
     final_output=""
     final_output += output[0].upper()
@@ -37,11 +43,37 @@ def post_process(output):
             final_output += output[i]
     return final_output
 
-def getSummary(input):
-    input = deEmojify(input)
-    # encode the text into tensor of integers using the tokenizer
-    inputs = t5_tokenizer.encode("summarize: " + input, return_tensors="pt", max_length=512, padding="max_length", truncation=True)
-    summary_ids = t5_model.generate(inputs, num_beams=int(2), no_repeat_ngram_size=3, length_penalty=2, min_length=120, max_length=300, early_stopping=True)
-    output = t5_tokenizer.decode(summary_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
-    final_output = post_process(output)
+def get_chunks(input, max_chunk_size = 300):
+    
+    sentences = input.split('<eos>')
+    current_chunk = 0 
+    chunks = []
+    for sentence in sentences:
+        if len(chunks) == current_chunk + 1: 
+            if len(chunks[current_chunk]) + len(sentence.split(' ')) <= max_chunk_size:
+                chunks[current_chunk].extend(sentence.split(' '))
+            else:
+                current_chunk += 1
+                chunks.append(sentence.split(' '))
+        else:
+            # print(current_chunk)
+            chunks.append(sentence.split(' '))
+
+    for chunk_id in range(len(chunks)):
+        chunks[chunk_id] = ' '.join(chunks[chunk_id])
+
+    return chunks
+
+def getSummary(text):
+    text = pre_process(text)
+    chunks = get_chunks(text)
+    
+    final_output = ""
+    for input in chunks:
+        inputs = t5_tokenizer.encode("summarize: " + input, return_tensors="pt", max_length=512, padding="max_length", truncation=True)
+        summary_ids = t5_model.generate(inputs, num_beams=int(2), no_repeat_ngram_size=3, length_penalty=2, min_length=30, max_length=120, early_stopping=True)
+        output = t5_tokenizer.decode(summary_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
+        final_output = post_process(output)
+        final_output = final_output + " " + output
+    
     return final_output
